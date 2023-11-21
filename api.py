b8059c6ff55d105d11b50ca93f2260aa29d7ab9c
Jaysonzhao <jaysonzhao@vip.qq.com>
2023-11-21 15:54:43 +0800

    Update api.py

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
 import os
 import transformers
 import mlflow
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
 from flask import Flask
 
 # Set environnment variables
@@ -13,7 +15,8 @@ mlflow.set_tracking_uri(MLFLOW_ROUTE)
 #TODO: replace with parameters
 logged_model = 'runs:/ff87bd85ce624433b3945c25691f5117/chatbot'
 
-loaded_model = mlflow.pyfunc.load_model(logged_model)
+#loaded_model = mlflow.pyfunc.load_model(logged_model)
+mlflow.artifacts.download_artifacts(artifact_uri=logged_model, dst_path='servepath')
 
 # Creation of the Flask app
 app = Flask(__name__)
@@ -23,8 +26,21 @@ app = Flask(__name__)
 @app.route('/',methods=['POST', 'GET'])
 # Return predictions of inference using Iris Test Data
 def prediction():
+ 
+    #mes = loaded_model.predict("Which kubernetes should should I use? Red Hat or VMWare?")
+    question = "Which kubernetes should should I use? Red Hat or VMWare?"
+    tokenizer = AutoTokenizer.from_pretrained("./servepath/chatbot/components/tokenizer")
+    model = AutoModelForCausalLM.from_pretrained("./servepath/chatbot/model")
+    new_user_input_ids = tokenizer.encode(question + tokenizer.eos_token, return_tensors='pt')
 
-    mes = loaded_model.predict("Which kubernetes should should I use? Red Hat or VMWare?")
+    # append the new user input tokens to the chat history
+    bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
+
+    # generated a response while limiting the total chat history to 1000 tokens, 
+    chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+
+    # pretty print last ouput tokens from bot
+    mes = format(tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True))
 
     return {'response': mes}
 
