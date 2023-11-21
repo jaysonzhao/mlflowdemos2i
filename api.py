4bd70e9552dd239a5c1fe82e66df2d0d3754accf
Jaysonzhao <jaysonzhao@vip.qq.com>
2023-11-21 16:57:36 +0800

    Update api.py

--- api.py
+++ api.py
@@ -34,7 +34,7 @@ def prediction():
     new_user_input_ids = tokenizer.encode(question + tokenizer.eos_token, return_tensors='pt')
 
     # append the new user input tokens to the chat history
-    bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
+    bot_input_ids = new_user_input_ids
 
     # generated a response while limiting the total chat history to 1000 tokens, 
     chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
