ba85548ef5a6d3ba27b5dde5c0cb713d3226dd69
Jaysonzhao <jaysonzhao@vip.qq.com>
2023-11-21 15:02:21 +0800

    Update api.py

--- api.py
+++ api.py
@@ -9,7 +9,8 @@ MLFLOW_MODEL = os.getenv("MLFLOW_MODEL")
 # Loading model
 print("Loading model from: {}".format(MLFLOW_ROUTE))
 mlflow.set_tracking_uri(MLFLOW_ROUTE)
-mlflow.set_experiment("dialogpt")
+
+#TODO: replace with parameters
 logged_model = 'runs:/ff87bd85ce624433b3945c25691f5117/chatbot'
 
 loaded_model = mlflow.pyfunc.load_model(logged_model)
