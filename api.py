e7dd2e0bd02e07c9b62eccae17ddbb91221249cc
Jaysonzhao <jaysonzhao@vip.qq.com>
2023-11-24 14:36:28 +0800

    Update api.py

--- api.py
+++ api.py
@@ -8,16 +8,17 @@ from flask import request
 
 # Set environnment variables
 MLFLOW_ROUTE = os.getenv("MLFLOW_ROUTE")
-MLFLOW_MODEL = os.getenv("MLFLOW_MODEL")
+
 # Loading model
 print("Loading model from: {}".format(MLFLOW_ROUTE))
 mlflow.set_tracking_uri(MLFLOW_ROUTE)
 
+MLFLOW_MODEL = os.getenv("MLFLOW_MODEL")
 #TODO: replace with parameters
-logged_model = 'runs:/ff87bd85ce624433b3945c25691f5117/chatbot'
+#logged_model = 'runs:/ff87bd85ce624433b3945c25691f5117/chatbot'
 
 #loaded_model = mlflow.pyfunc.load_model(logged_model)
-mlflow.artifacts.download_artifacts(artifact_uri=logged_model, dst_path='servepath')
+mlflow.artifacts.download_artifacts(artifact_uri=MLFLOW_MODEL, dst_path='servepath')
 
 # Creation of the Flask app
 app = Flask(__name__)
