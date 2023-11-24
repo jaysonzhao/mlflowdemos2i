a4c67cbcdfcfaa3e55174b8a9dd5b9216ab38165
Jaysonzhao <jaysonzhao@vip.qq.com>
2023-11-24 22:20:36 +0800

    Update apis3.py

--- apis3.py
+++ apis3.py
@@ -60,16 +60,14 @@ def prediction():
     tokenizer = AutoTokenizer.from_pretrained("./servepath/components/tokenizer")
     model = AutoModelForCausalLM.from_pretrained("./servepath/model")
  
-    new_user_input_ids = tokenizer.encode(question + tokenizer.eos_token, return_tensors='pt')
+    user_input_ids = tokenizer.encode(question + tokenizer.eos_token, return_tensors='pt')
 
-    # append the new user input tokens to the chat history
-    bot_input_ids = new_user_input_ids
 
     # generated a response while limiting the total chat history to 1000 tokens, 
-    chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+    answer_ids = model.generate(user_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
 
-    # pretty print last ouput tokens from bot
-    mes = format(tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True))
+    # pretty print tokens from bot
+    mes = format(tokenizer.decode(answer_ids[:, user_input_ids.shape[-1]:][0], skip_special_tokens=True))
 
     return {'response': mes}
 
